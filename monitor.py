5b85823a2346a19e6381ea71548edbd48d25e2c3
fenicexxiii-prog <fenicexxiii@gmail.com>
2026-06-09 15:13:56 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,31 +1,19 @@
 import requests
-import time
 
 URL = "https://www.booking.com/hotel/it/primaverammare.it.html"
 
-BOT_TOKEN = 8998464103:AAESkI9kP3w-PKwVD9vaEqjNJ_014PQRksM/sendMessage?chat_id=7985695301&text=Test%20OK
+BOT_TOKEN = "INSERISCI_TOKEN"
 CHAT_ID = "7985695301"
-
-last_state = None
 
 def send(msg):
     requests.post(
-        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-        data={"chat_id": CHAT_ID, "text": msg}
+        f"https://api.telegram.org/bot=8998464103:AAESkI9kP3w-PKwVD9vaEqjNJ_014PQRksM/sendMessage?chat_id=7985695301&text=Test%20OK/sendMessage",
+        data={"chat_id": CHAT_ID,7985695301: msg}
     )
 
-while True:
-    try:
-        r = requests.get(URL, headers={"User-Agent": "Mozilla/5.0"})
-        text = r.text
+def check():
+    r = requests.get(URL, headers={"User-Agent": "Mozilla/5.0"})
+    if "review" in r.text.lower():
+        send("⭐ Test Booking monitor attivo")
 
-        if last_state and text != last_state:
-            send("⭐ Possibile nuova recensione su Booking!")
-
-        last_state = text
-
-        time.sleep(3600)
-
-    except Exception as e:
-        send(f"Errore script: {e}")
-        time.sleep(3600)
+check()
